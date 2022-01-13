c9e41f3d8789ee0889383be59fddd020e219158f
André Ezequiel <andrelucasmartins@hotmail.com>
2022-01-12 23:29:58 -0300

    Add script for scrapping on the website

---
+++ README.md
@@ -0,0 +1,24 @@
+# Webscripping
+
+O script desenvolvido, lista às músicas mais tocadas.
+Conteúdo retirado de website:
+ [https://maistocadas.mus.br/pagode-samba/](https://maistocadas.mus.br/pagode-samba/)
+
+
+## Instalar libs
+```python
+$ py install requests
+```
+
+```python
+$ py install bs4
+```
+
+```python
+$ py install html5lib
+```
+## Para rodar o projeto use o comando abaixo.
+
+```python
+$ python scraper.py
+```
---
+++ scraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://maistocadas.mus.br/pagode-samba/"
+
+headers =  {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+}
+
+site = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(site.content, 'html5lib')
+
+videos =  soup.find('div', attrs = {'class': 'lista'})
+
+# name = videos.find('ol')
+print(videos.prettify())
